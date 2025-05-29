104e59bc23b6f64587d7e461d314d1bbed3a3306
AsherDoesTechs <Asherbasco93@gmail.com>
2025-05-29 21:27:36 +0800

    Added scrollRestoration!

--- script.js
+++ script.js
@@ -106,3 +106,9 @@ document.addEventListener("DOMContentLoaded", () => {
     detectRetina: true,
   });
 });
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
